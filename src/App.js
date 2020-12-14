3d7f0cc19cc7b4e69975f9d2cccc7f99693af874
transformator98 <vanushka16@gmail.com>
2020-12-15 01:01:22 +0200

    adedd delete contact

--- src/App.js
+++ src/App.js
@@ -1,79 +1,71 @@
 import { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+import Contacts from './Contact';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
 class App extends Component {
   state = {
-    contacts: [],
-    name: '',
-    number: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  repeatContact = (name, number) => {
+    const { contacts } = this.state;
+    const repeatName = !!contacts.find(contact => contact.name === name);
+    return repeatName;
   };
 
-  addContact = ({ name, number }) => {
-    console.log('name', name);
-    console.log('number', number);
+  addContact = (name, number) => {
     const contact = {
       id: uuidv4(),
       name,
       number,
     };
+    const repeat = this.repeatContact(name, number);
+
+    !name || !number
+      ? alert('You did not enter a name or number')
+      : repeat
+      ? alert(`${name} is already exist!`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  deleteContact = contactId => {
     this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
+      contacts: contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(this.state);
-
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
   };
 
   render() {
-    const { name, number } = this.state;
+    const { contacts, filter } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter),
+    );
 
     return (
       <div>
         <h1>phonebook</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            Number
-            <input
-              type="text"
-              name="number"
-              value={number}
-              onChange={this.handleInputChange}
-            />
-          </label>
-
-          <button type="submit">Add contact</button>
-        </form>
+        <Contacts onAddSubmit={this.addContact} onRepeat={this.repeatContact} />
         <h2>contact</h2>
-        <ul>
-          {this.state.contacts.map(contact => (
-            <li>
-              <p>
-                {contact.name}: {contact.number}
-              </p>
-            </li>
-          ))}
-        </ul>
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
--- src/Contact/Contacts.js
+++ src/Contact/Contacts.js
@@ -6,8 +6,49 @@ class Contacts extends Component {
     number: '',
   };
 
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    const { name, number } = this.state;
+    event.preventDefault();
+    this.props.onAddSubmit(name, number);
+    this.props.onRepeat(name, number);
+
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
   render() {
-    return;
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="number"
+            name="number"
+            value={number}
+            onChange={this.handleInputChange}
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
   }
 }
 
